c0a32aaeef8d5e1800c74ff266dcb0ceade43070
DB <108250593+DB00001010@users.noreply.github.com>
2025-03-11 23:31:32 -0700

    added persistence and config

--- config.lua
+++ config.lua
@@ -1,5 +1,18 @@
-Config = {
-    AutoLockDistance = 10.0, -- Distance to lock the vehicle
-    AutoUnlockDistance = 5.0, -- Distance to unlock the vehicle
-    DistanceCheckInterval = 1000 -- Timer interval for distance checks (in ms)
+Config = {}
+
+Config.AutoLockDistance = 10.0 -- Distance to lock the vehicle
+
+Config.AutoUnlockDistance = 5.0 -- Distance to unlock the vehicle
+
+Config.DistanceCheckInterval = 1000 -- Timer interval for distance checks (in ms)
+
+Config.LastVehicleTrackTime = 500 -- Timer interval for last vehicle tracking (in ms)
+
+Config.Persistent = true
+
+Config.Notifications = {
+    autolock_activated = "Auto-Lock Activated",
+    autolock_deactivated = "Auto-Lock Deactivated",
+    vehicle_autolocked = "Vehicle Auto-Locked",
+    vehicle_autounlocked = "Vehicle Auto-Unlocked",
 }
