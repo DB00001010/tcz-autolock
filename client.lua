7c4cf53d05013365a346797c4739cfc723c37312
DB <108250593+DB00001010@users.noreply.github.com>
2025-03-11 23:31:51 -0700

    added persistence and config

--- client.lua
+++ client.lua
@@ -6,7 +6,11 @@ local lockState = false
 -- Sync Auto-Lock toggle state
 RegisterNetEvent('tcz-autolock:toggle', function(state)
     autoLockEnabled = state
-    QBCore.Functions.Notify(state and "Auto-Lock Activated" or "Auto-Lock Deactivated", "success")
+    QBCore.Functions.Notify(state and Config.Notifications.autolock_activated or Config.Notifications.autolock_deactivated, "success")
+
+    -- Play sound effect when toggling Auto-Lock
+    local sound, soundSet = state and "CONFIRM_BEEP" or "Beep_Red", state and "HUD_MINI_GAME_SOUNDSET" or "DLC_HEIST_HACKING_SNAKE_SOUNDS"
+    PlaySoundFrontend(-1, sound, soundSet, true)
 end)
 
 -- Track the last driven vehicle
@@ -19,7 +23,7 @@ CreateThread(function()
             lastDriven = { netId = NetworkGetNetworkIdFromEntity(veh), plate = GetVehicleNumberPlateText(veh) }
         end
 
-        Wait(500) -- Polling interval for tracking last driven vehicle
+        Wait(Config.LastVehicleTrackTime) -- Polling interval for tracking last driven vehicle
     end
 end)
 
@@ -40,13 +44,13 @@ CreateThread(function()
                 if distance > Config.AutoLockDistance and occupantCount == 0 and not lockState then
                     LockVehicle(lastVeh, true) -- Lock the vehicle
                     lockState = true
-                    QBCore.Functions.Notify("Vehicle auto-locked.", "success")
+                    QBCore.Functions.Notify(Config.Notifications.vehicle_autolocked, "success")
 
                 -- Unlock vehicle if the player is within AutoUnlockDistance
                 elseif distance < Config.AutoUnlockDistance and lockState then
                     LockVehicle(lastVeh, false) -- Unlock the vehicle
                     lockState = false
-                    QBCore.Functions.Notify("Vehicle auto-unlocked.", "success")
+                    QBCore.Functions.Notify(Config.Notifications.vehicle_autounlocked, "success")
                 end
             else
                 -- Reset if the vehicle no longer exists
@@ -75,3 +79,13 @@ function LockVehicle(veh, lock)
     local sound = lock and "Remote_Control_Close" or "Remote_Control_Open"
     PlaySoundFromEntity(-1, sound, veh, "PI_Menu_Sounds", 0, 0)
 end
+
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function ()
+    if Config.Persistent then
+        QBCore.Functions.GetPlayerData(function(PlayerData)
+            if PlayerData.metadata['tcz-autolock'] then
+                autoLockEnabled = PlayerData.metadata['tcz-autolock'] or false
+            end
+        end)
+    end
+end)
